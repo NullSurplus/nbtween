1c36e4cff3d11a11d35d774e6dbc1324299ef9a0
Derek Forgione <derekforgione@gmail.com>
2022-10-21 05:56:20 -0700

    Updated app and added some test code.

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,27 @@
+use crate::nbtween::{
+    NbtFile,
+};
+use rfd;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
+pub struct NbtweenApp {
     #[serde(skip)]
-    value: f32,
+    files: Vec<NbtFile>,
+    selected_file: Option<usize>,
 }
 
-impl Default for TemplateApp {
+impl Default for NbtweenApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            files: Vec::new(),
+            selected_file: None,
         }
     }
 }
 
-impl TemplateApp {
+impl NbtweenApp {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
@@ -36,7 +37,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for NbtweenApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -45,18 +46,30 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self { .. } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
                 ui.menu_button("File", |ui| {
+                    if ui.button("Open NBT File").clicked() {
+                        let paths = rfd::FileDialog::new()
+                            .set_directory("/")
+                            .pick_files();
+                        if let Some(files) = paths {
+                            self.files.extend(
+                                files.iter()
+                                    .map(|path| NbtFile::load(path))
+                                    .filter_map(|file| file.ok())
+                            );
+                        }
+                    }
+                    #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!=
                     if ui.button("Quit").clicked() {
                         _frame.close();
                     }
@@ -64,44 +77,14 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
-        });
+        // egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            
+        // });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+            // This is where the main NBT editor will be built.
+            let filecount = self.files.len().to_string();
+            ui.label(filecount);
         });
 
         if false {
